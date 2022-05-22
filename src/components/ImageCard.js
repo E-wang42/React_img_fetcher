5c03af282ffee2ad139acb60046dc128d7161024
eric <ericwang42@outlook.com>
2022-05-21 22:10:29 -0700

    last commit?

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,18 +4,28 @@ class ImageCard extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log();
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10 + 1);
+
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
