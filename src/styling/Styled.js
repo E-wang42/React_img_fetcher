1eea6cb09db6f1b971a75043c1c8fc8e8ce1fc10
eric <ericwang42@outlook.com>
2022-05-29 22:42:49 -0700

    update

--- src/styling/Styled.js
+++ src/styling/Styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const PageCentered = styled.main`
   margin: 0 auto;
-  background-color: hotpink;
+  background: linear-gradient(to right, #fcb045, #fd1d1d, #833ab4);
   height: 100vh;
 `;
 
